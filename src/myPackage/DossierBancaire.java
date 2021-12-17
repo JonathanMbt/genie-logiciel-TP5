22fe15f318655f6b731060d9889938ba9efde1cf
usrlocal <usrlocal@GA319-FE06.ad.univ-angers.fr>
2021-12-17 10:01:44 +0100

    documentation Dossier Bancaire

--- src/myPackage/DossierBancaire.java
+++ src/myPackage/DossierBancaire.java
@@ -10,14 +10,14 @@ public class DossierBancaire
 	//Constructeur
     public DossierBancaire()
     {
-    	compteC = new Compte(0);
-    	compteE = new CompteEpargne(0);
+    	compteC = new Compte(0); // initialisation du compte courant à 0€
+    	compteE = new CompteEpargne(0); // initialisation du compte courant à 0€
     }
 
     public void depot(double value)
     {
-    	compteC.addSolde(0.4 * value);
-    	compteE.addSolde(0.6 * value);
+    	compteC.addSolde(0.4 * value); // 40% reversé sur le compte courant
+    	compteE.addSolde(0.6 * value); // 60% reversé sur le compte épargne
     }
     
     public double getSoldeTotal()
